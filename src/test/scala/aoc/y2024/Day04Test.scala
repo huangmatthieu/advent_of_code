3c616ee303dc3389f3d991d75a5d806d3eba4b55
huangmatthieu <mgspw92@gmail.com>
2024-12-05 00:25:11 +0100

    add day04

---
+++ src/test/scala/aoc/y2024/Day04Test.scala
@@ -0,0 +1,30 @@
+package aoc.y2024
+
+import org.scalatest.flatspec.AnyFlatSpec
+import org.scalatest.matchers.should.Matchers
+import org.scalatest.funsuite.AnyFunSuiteLike
+
+class Day04Test extends AnyFlatSpec with Matchers {
+
+  private val testInput = List(
+    "MMMSXXMASM",
+    "MSAMXMSMSA",
+    "AMXSXMAAMM",
+    "MSAMASMSMX",
+    "XMASAMXAMM",
+    "XXAMMXXAMA",
+    "SMSMSASXSS",
+    "SAXAMASAAA",
+    "MAMMMXMMMM",
+    "MXMXAXMASX"
+  ).map(_.toList)
+
+  "Day04 - 2024" should "solve first part" in {
+    Day04.part1(testInput) should equal(18)
+  }
+
+  it should "solve second part" in {
+    Day04.part2(testInput) should equal(9)
+  }
+
+}
